d2ef1c84b2171994731ca26bcaab07abde643141
N. Geisler <ngeisler@fachschaft.informatik.tu-darmstadt.de>
2019-10-13 12:33:19 +0200

    add app for project dashboard

---
+++ AKDashboard/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AkdashboardConfig(AppConfig):
+    name = 'AKDashboard'
--- AKPlanning/settings.py
+++ AKPlanning/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'AKModel.apps.AkmodelConfig',
+    'AKDashboard.apps.AkdashboardConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
