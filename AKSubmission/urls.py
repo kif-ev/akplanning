5f6407a66b633b1577f8a2eae567d48023025b9c
Benjamin Hättasch <benjamin.haettasch@cs.tu-darmstadt.de>
2019-10-18 01:37:44 +0200

    Add more views to submission component and improve existing one
    Add dynamic title handling to base html
    Add more information to submission overview
    Move AK list to own file for use in other pages
    Add detail view for AK
    Add views with AK overview list as well as filtered lists (by type and tag)
    Add corresponding German translations

--- AKSubmission/urls.py
+++ AKSubmission/urls.py
@@ -9,6 +9,10 @@ urlpatterns = [
         '<slug:event_slug>/',
         include([
             path('submission/', views.SubmissionOverviewView.as_view(), name='submission_overview'),
+            path('submission/ak/<int:pk>', views.AKDetailView.as_view(), name='ak_detail'),
+            path('submission/aks/', views.AKListView.as_view(), name='ak_list'),
+            path('submission/aks/type/<int:type_pk>', views.AKListByTypeView.as_view(), name='ak_list_by_type'),
+            path('submission/aks/tag/<int:tag_pk>', views.AKListByTagView.as_view(), name='ak_list_by_tag'),
         ])
     ),
 ]
--- AKSubmission/views.py
+++ AKSubmission/views.py
@@ -1,7 +1,10 @@
-from django.views.generic import ListView
+from django.http import Http404
+from django.views.generic import ListView, DetailView
 
-from AKModel.models import AK
+from AKModel.models import AK, AKType, AKTag
 from AKModel.views import FilterByEventSlugMixin
+
+from django.utils.translation import gettext_lazy as _
 
 
 class SubmissionOverviewView(FilterByEventSlugMixin, ListView):
@@ -9,3 +12,49 @@ class SubmissionOverviewView(FilterByEventSlugMixin, ListView):
     context_object_name = "AKs"
     template_name = "AKSubmission/submission_overview.html"
     ordering = ["type"]
+
+
+class AKDetailView(DetailView):
+    model = AK
+    context_object_name = "ak"
+    template_name = "AKSubmission/ak_detail.html"
+
+
+class AKListView(FilterByEventSlugMixin, ListView):
+    model = AK
+    context_object_name = "AKs"
+    template_name = "AKSubmission/ak_list.html"
+    filter_condition_string = ""
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context["types"] = AKType.objects.all()
+        context["tags"] = AKTag.objects.all()
+        context["filter_condition_string"] = self.filter_condition_string
+        return context
+
+
+class AKListByTypeView(AKListView):
+    type = None
+
+    def get_queryset(self):
+        # Find type based on event slug
+        try:
+            self.type = AKType.objects.get(pk=self.kwargs['type_pk'])
+            self.filter_condition_string = f"{_('Type')} = {self.type.name}"
+        except AKType.DoesNotExist:
+            raise Http404
+        return super().get_queryset().filter(type=self.type)
+
+
+class AKListByTagView(AKListView):
+    tag = None
+
+    def get_queryset(self):
+        # Find type based on event slug
+        try:
+            self.tag = AKTag.objects.get(pk=self.kwargs['tag_pk'])
+            self.filter_condition_string = f"{_('Tag')} = {self.tag.name}"
+        except AKTag.DoesNotExist:
+            raise Http404
+        return super().get_queryset().filter(tags=self.tag)
