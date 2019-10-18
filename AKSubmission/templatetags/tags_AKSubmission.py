b4bf68fd07c8e58c03408db1acd62dd4727c1ed6
Benjamin Hättasch <benjamin.haettasch@cs.tu-darmstadt.de>
2019-10-18 16:29:34 +0200

    Introduce and use custom templatetags for AKSubmission
    Created filter to represent values using fontawesome icons
    Added template tags (and corresponding templates) for category and tag lists and badges
    Adapted existing view templates to use these tags
    Fixed presentation of AK indicator

---
+++ AKSubmission/templatetags/tags_AKSubmission.py
@@ -0,0 +1,26 @@
+from django import template
+from fontawesome.templatetags.fontawesome import fontawesome_icon
+
+register = template.Library()
+
+
+@register.filter
+def bool_symbol(bool_val):
+    if bool_val:
+        return fontawesome_icon("check")
+    return fontawesome_icon("times")
+
+
+@register.inclusion_tag("AKSubmission/tags_list.html")
+def tag_list(tags, event_slug):
+    return {"tags": tags, "event_slug": event_slug}
+
+
+@register.inclusion_tag("AKSubmission/category_list.html")
+def category_list(categories, event_slug):
+    return {"categories": categories, "event_slug": event_slug}
+
+
+@register.inclusion_tag("AKSubmission/category_linked_badge.html")
+def category_linked_badge(category, event_slug):
+    return {"category": category, "event_slug": event_slug}
