9bc0b49452b3bfc55d28e4ad9311344d8f8dba16
N. Geisler <ngeisler@fachschaft.informatik.tu-darmstadt.de>
2019-10-19 01:30:04 +0200

    select form fields for ak submission
    exclude interest
    exclude track

--- AKSubmission/forms.py
+++ AKSubmission/forms.py
@@ -6,4 +6,17 @@ from AKModel.models import AK
 class AKForm(forms.ModelForm):
     class Meta:
         model = AK
-        fields = '__all__'
+        fields = ['name',
+                  'short_name',
+                  'link',
+                  'owners',
+                  'description',
+                  'category',
+                  'tags',
+                  'reso',
+                  'present',
+                  'requirements',
+                  'conflicts',
+                  'prerequisites',
+                  'notes',
+                  'event']
