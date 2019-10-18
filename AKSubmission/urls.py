e13c5c45ddb5d80a71e21f9e18134e2d5fbebf5b
N. Geisler <ngeisler@fachschaft.informatik.tu-darmstadt.de>
2019-10-18 11:59:22 +0200

    merge url prefix

--- AKSubmission/urls.py
+++ AKSubmission/urls.py
@@ -6,13 +6,13 @@ app_name = "submit"
 
 urlpatterns = [
     path(
-        '<slug:event_slug>/',
+        '<slug:event_slug>/submission/',
         include([
-            path('submission/', views.SubmissionOverviewView.as_view(), name='submission_overview'),
-            path('submission/ak/<int:pk>', views.AKDetailView.as_view(), name='ak_detail'),
-            path('submission/aks/', views.AKListView.as_view(), name='ak_list'),
-            path('submission/aks/type/<int:type_pk>', views.AKListByTypeView.as_view(), name='ak_list_by_type'),
-            path('submission/aks/tag/<int:tag_pk>', views.AKListByTagView.as_view(), name='ak_list_by_tag'),
+            path('', views.SubmissionOverviewView.as_view(), name='submission_overview'),
+            path('ak/<int:pk>', views.AKDetailView.as_view(), name='ak_detail'),
+            path('aks/', views.AKListView.as_view(), name='ak_list'),
+            path('aks/type/<int:type_pk>', views.AKListByTypeView.as_view(), name='ak_list_by_type'),
+            path('aks/tag/<int:tag_pk>', views.AKListByTagView.as_view(), name='ak_list_by_tag'),
         ])
     ),
 ]
