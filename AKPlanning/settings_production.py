5e2e734532c3e1d2cc4eb008c251ee59d9b1b824
N. Geisler <ngeisler@fachschaft.informatik.tu-darmstadt.de>
2019-10-25 17:11:14 +0200

    add production DB config

--- AKPlanning/settings_production.py
+++ AKPlanning/settings_production.py
@@ -4,7 +4,6 @@ First, it imports all default settings, then overrides respective ones.
 Secrets are stored in and imported from an additional file, not set under version control.
 """
 
-from AKPlanning.settings import *
 import AKPlanning.settings_secrets as secrets
 
 
@@ -16,4 +15,22 @@ ALLOWED_HOSTS = secrets.HOSTS
 
 SECRET_KEY = secrets.SECRET_KEY
 
-# TODO: DB, chaching, CSRF etc.
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+### DATABASE ###
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'NAME': secrets.DB_NAME,
+        'USER': secrets.DB_USER,
+        'PASSWORD': secrets.DB_PASSWORD,
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
+    }
+}
+
+# TODO: caching
