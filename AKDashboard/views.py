5e3c8790c2ecac027db6ea5e42ad08c1bc634939
Benjamin Hättasch <benjamin.haettasch@cs.tu-darmstadt.de>
2019-10-28 18:41:35 +0100

    Fix lazy loading of redirect url in dashboard view

--- AKDashboard/views.py
+++ AKDashboard/views.py
@@ -6,5 +6,7 @@ from AKModel.models import Event
 
 class TopLevelRedirectView(RedirectView):
     is_permanent = False
-    url = reverse_lazy('submit:submission_overview',
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse_lazy('submit:submission_overview',
                        kwargs={'event_slug': Event.objects.filter(active=True).last().slug})
