c3f955b57f320cc0749e35c9212db37012c918b5
N. Geisler <ngeisler@fachschaft.informatik.tu-darmstadt.de>
2019-10-30 17:47:09 +0100

    start work on AK Wall

--- AKPlan/urls.py
+++ AKPlan/urls.py
@@ -1,3 +1,15 @@
+from django.urls import include, path
+
+from AKPlan import views
+
 app_name = 'plan'
+
 urlpatterns = [
+    path(
+        '<slug:event_slug>/plan/',
+        include([
+            path('', views.PlanView.as_view(), name='ak_plan')
+        ])
+    ),
+
 ]
--- AKPlan/views.py
+++ AKPlan/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.views.generic import TemplateView
+
+from AKModel.views import EventSlugMixin
+
+
+class PlanView(EventSlugMixin, TemplateView):
+    template_name = 'AKPlan/plan.html'
