f2a1e000ecc77a52165e16bf34f5bb3e90e8b84a
N. Geisler <ngeisler@fachschaft.informatik.tu-darmstadt.de>
2019-10-30 22:38:44 +0100

    improve dashboard:
    send root url to dashboard
    link AK submission

--- AKDashboard/views.py
+++ AKDashboard/views.py
@@ -10,8 +10,7 @@ class TopLevelRedirectView(RedirectView):
     is_permanent = False
 
     def get_redirect_url(self, *args, **kwargs):
-        return reverse_lazy('submit:submission_overview',
-                            kwargs={'event_slug': Event.objects.filter(active=True).last().slug})
+        return reverse_lazy('dashboard:dashboard')
 
 
 class DashboardView(TemplateView):
