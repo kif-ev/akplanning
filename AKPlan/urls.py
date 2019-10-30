4e103823c51397a3528a7a3283ac159e0a7eecaa
N. Geisler <ngeisler@fachschaft.informatik.tu-darmstadt.de>
2019-10-30 18:29:01 +0100

    add api urls

--- AKPlan/urls.py
+++ AKPlan/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 
 from AKPlan import views
+from AKScheduling.urls import api_router
 
 app_name = 'plan'
 
@@ -8,7 +9,9 @@ urlpatterns = [
     path(
         '<slug:event_slug>/plan/',
         include([
-            path('', views.PlanView.as_view(), name='ak_plan')
+            path('', views.PlanView.as_view(), name='ak_plan'),
+            path('api', include(api_router.urls)),
+
         ])
     ),
 
