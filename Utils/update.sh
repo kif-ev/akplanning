0712995d6217fa08e4940d45999acf10b90ad5d3
N. Geisler <ngeisler@fachschaft.informatik.tu-darmstadt.de>
2020-05-12 17:26:32 +0200

    when updating, create data dump first
    closes #58

--- Utils/update.sh
+++ Utils/update.sh
@@ -15,6 +15,10 @@ if [ "$1" = "--prod" ]; then
     export DJANGO_SETTINGS_MODULE=AKPlanning.settings_production
 fi
 
+# before potentially breaking anything, create a data backup
+mkdir -p backups/
+python manage.py dumpdata --indent=2 > "backups/$(date +"%Y%m%d%H%M")_datadump.json" --traceback
+
 git pull
 pip install --upgrade setuptools pip wheel
 pip install --upgrade -r requirements.txt
