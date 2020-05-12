f84dfd5bcc4d085a17dd79a953f5536afd7a38fd
Benjamin Hättasch <benjamin.haettasch@fachschaft.informatik.tu-darmstadt.de>
2020-05-12 02:24:19 +0200

    Add per-event dashboards

--- AKDashboard/urls.py
+++ AKDashboard/urls.py
@@ -6,4 +6,5 @@ app_name = "dashboard"
 
 urlpatterns = [
     path('', views.DashboardView.as_view(), name="dashboard"),
+    path('<slug:slug>/', views.DashboardEventView.as_view(), name='dashboard_event'),
 ]
