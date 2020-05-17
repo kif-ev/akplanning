a2cc549876b238f83bc0a4bd4c3dc51d4a5c9d22
N. Geisler <ngeisler@fachschaft.informatik.tu-darmstadt.de>
2020-05-17 22:36:43 +0200

    introduce virtual rooms

--- AKOnline/admin.py
+++ AKOnline/admin.py
@@ -1,1 +1,9 @@
-# Register your models here.
+from django.contrib import admin
+
+from AKModel.admin import RoomAdmin
+from AKOnline.models import VirtualRoom
+
+
+@admin.register(VirtualRoom)
+class VirtualRoomAdmin(RoomAdmin):
+    model = VirtualRoom
--- AKOnline/models.py
+++ AKOnline/models.py
@@ -1,1 +1,14 @@
-# Create your models here.
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from AKModel.models import Event, Room
+
+
+class VirtualRoom(Room):
+    """ A virtual room where an AK can be held.
+    """
+    url = models.URLField(verbose_name=_("URL"), help_text=_("URL to the room or server"), blank=True)
+
+    class Meta:
+        verbose_name = _('Virtual Room')
+        verbose_name_plural = _('Virtual Rooms')
