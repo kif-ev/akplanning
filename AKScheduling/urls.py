75e6886e2d3f901da3d658d267380cda73b117ed
Benjamin Hättasch <benjamin.haettasch@fachschaft.informatik.tu-darmstadt.de>
2021-05-09 11:51:40 +0200

    Unify scheduling admin url structure (now all admin/AKModel/event/slug/action)

--- AKScheduling/urls.py
+++ AKScheduling/urls.py
@@ -3,17 +3,12 @@ from django.urls import path
 from AKScheduling.views import SchedulingAdminView, UnscheduledSlotsAdminView, TrackAdminView
 
 
-def get_admin_urls_slot(admin_site):
+def get_admin_urls_scheduling(admin_site):
     return [
         path('<slug:event_slug>/schedule/', admin_site.admin_view(SchedulingAdminView.as_view()),
              name="schedule"),
         path('<slug:event_slug>/unscheduled/', admin_site.admin_view(UnscheduledSlotsAdminView.as_view()),
              name="slots_unscheduled"),
-    ]
-
-
-def get_admin_urls_track(admin_site):
-    return [
-        path('<slug:event_slug>/manage/', admin_site.admin_view(TrackAdminView.as_view()),
+        path('<slug:event_slug>/tracks/', admin_site.admin_view(TrackAdminView.as_view()),
              name="tracks_manage"),
     ]
