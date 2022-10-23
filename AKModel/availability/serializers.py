64d6fe6404ee0db5cb56b39a3a4495fc177609ee
Benjamin Hättasch <benjamin.haettasch@fachschaft.informatik.tu-darmstadt.de>
2022-10-23 21:40:27 +0200

    Port availabilities editor to fullcalendar v5 and adjust usage
    This used the common fullcalendar v5 instance instead of a special v3 version only for this editor
    Improve visualization of availability deletion
    Improve (event specific) timezone handling and remove dependency to moment-timezones

--- AKModel/availability/serializers.py
+++ AKModel/availability/serializers.py
@@ -2,6 +2,7 @@
 # Copyright 2017-2019, Tobias Kunze
 # Original Copyrights licensed under the Apache License, Version 2.0 http://www.apache.org/licenses/LICENSE-2.0
 # Changes are marked in the code
+from django.utils import timezone
 from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 
@@ -10,9 +11,19 @@ from AKModel.availability.models import Availability
 
 class AvailabilitySerializer(ModelSerializer):
     allDay = SerializerMethodField()
+    start = SerializerMethodField()
+    end = SerializerMethodField()
 
     def get_allDay(self, obj):
         return obj.all_day
+
+    # Use already localized strings in serialized field
+    # (default would be UTC, but that would require heavy timezone calculation on client side)
+    def get_start(self, obj):
+        return timezone.localtime(obj.start, obj.event.timezone).strftime("%Y-%m-%dT%H:%M:%S")
+
+    def get_end(self, obj):
+        return timezone.localtime(obj.end, obj.event.timezone).strftime("%Y-%m-%dT%H:%M:%S")
 
     class Meta:
         model = Availability
