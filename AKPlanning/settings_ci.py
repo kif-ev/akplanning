457f044d168a3d3813db98485ccb4fcbf4edf5bb
Benjamin Hättasch <benjamin.haettasch@fachschaft.informatik.tu-darmstadt.de>
2022-12-02 22:08:14 +0100

    Make sure CI db uses UTF-8 charset

--- AKPlanning/settings_ci.py
+++ AKPlanning/settings_ci.py
@@ -15,7 +15,8 @@ DATABASES = {
         'USER': 'django',
         'PASSWORD': 'mysql',
         'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': "utf8mb4",
         },
         'TEST': {
             'NAME': 'test',
