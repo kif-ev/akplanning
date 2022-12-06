3261eea0d82bd7d07c0671e3c75209cc5ff442e5
Benjamin Hättasch <benjamin.haettasch@fachschaft.informatik.tu-darmstadt.de>
2022-12-06 23:18:39 +0100

    Adapt CI settings for fixture loading

--- AKPlanning/settings_ci.py
+++ AKPlanning/settings_ci.py
@@ -16,10 +16,11 @@ DATABASES = {
         'PASSWORD': 'mysql',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': "utf8mb4",
         },
         'TEST': {
-            'NAME': 'test',
+            'NAME': 'tests',
+            'CHARSET': "utf8mb4",
+            'COLLATION': 'utf8mb4_unicode_ci',
         },
     }
 }
