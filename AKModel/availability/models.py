500013b325ff940979ddea6e0f81162edc53914d
Felix Blanke <felixblanke@uni-bonn.de>
2025-06-14 02:57:51 +0200

    Fix avail __str__ for anonymous participants

--- AKModel/availability/models.py
+++ AKModel/availability/models.py
@@ -94,7 +94,7 @@ class Availability(models.Model):
 
     def __str__(self) -> str:
         person = self.person.name if self.person else None
-        participant = self.participant.name if self.participant else None
+        participant = str(self.participant) if self.participant else None
         room = getattr(self.room, 'name', None)
         event = getattr(getattr(self, 'event', None), 'name', None)
         ak = getattr(self.ak, 'name', None)
